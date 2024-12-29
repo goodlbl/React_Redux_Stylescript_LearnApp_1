f369b8bb1cd0b57194fdfdbd03e982335b0ba891
goodlbl <Goodworldxyz@gmail.com>
2024-12-29 17:19:55 +0700

    lesson7 2

--- rrs-la-1/src/App.tsx
+++ rrs-la-1/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes} from 'react-router-dom';
+
 import './App.css';
+import './styles/sb-admin-2.min.css';
+import Login from './pages/Account/Login';
+import Admin from './pages/Admin/Admin';
+
 
 function App() {
   return (
     <div className="App" id='wrapper'>
-      
-
-        
+      <Router>
+        <Routes>
+          <Login></Login>
+          <Admin></Admin>
+        </Routes>
+      </Router>
     </div>
   );
 }
